e0450c719050dd301817c573e9aa8e66ee73d252
Dedy Akwety <dedyakwety@gmail.com>
2022-11-26 22:48:15 +0100

    design footer, recherche update

--- resources/views/pages/article/categorie.blade.php
+++ resources/views/pages/article/categorie.blade.php
@@ -16,9 +16,10 @@
 					@csrf
 					<select name="q" class="form-control mr-2" id="champ-recherche">
 						@foreach($modeles as $modele)
-							<option value="{{ $modele->id }}">{{ $modele->modele }}</option>
+							<option value="{{ $modele->modele }}">{{ $modele->modele }}</option>
 						@endforeach
 					</select>
+					<!--input type="text" name="q" class="form-control mr-2" id="champ-recherche"-->
 					<button class="btn btn-primary pt-2" id="btn">
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
 					  	<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
