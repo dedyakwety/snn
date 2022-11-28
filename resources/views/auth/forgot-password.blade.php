09d997c1d9f51b41bf1ebd9f8b0f7c9cdd2b1480
Dedy Akwety <dedyakwety@gmail.com>
2022-11-28 13:00:38 +0100

    mp oublier

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,27 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>SNN</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Colo Shop Template">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="{{ asset('styles/mp_oublier.css') }}">
+</head>
+<body>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('password.email') }}">
+    <div class="body-mp-oublier">
+        
+        <form action="{{ route('password.email') }}" method="POST" class="form">
             @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
+            <p>Entrez votre adresse email pour recevoir un code par mail enfin de reinitialiser votre mot de passe</p>
+            <div class="inputs">
+                <input type="email" name="email" class="input" placeholder="exemple@gmail.com" class="inpout" autofocus />
+                <button type="submit" class="boutton">Reinitialiser</button>
             </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+
+    </div>
+
+</body>
+</html>
