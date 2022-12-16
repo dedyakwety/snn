c366ebfd2a6d448df6537e3dd5cb8ce2c1723504
Dedy Akwety <dedyakwety@gmail.com>
2022-12-16 09:07:54 +0100

    ajout

--- resources/views/pages/commande/commande_reussie.blade.php
+++ resources/views/pages/commande/commande_reussie.blade.php
@@ -4,7 +4,7 @@
 
 	<div class="div-commade-reussie">
 		<div class="div-1">
-			Commande réussie avec succès, nous vous contacterons pour vous confirmé la livraison. Vous aurez la facture lors de la livraison
+			Commande réussie avec succès, nous vous contacterons pour vous confirmé la livraison. Vous aurez la facture lors de la livraisons
 		</div>
 		<div class="div-2">
 			<a href="{{ route('index') }}">
