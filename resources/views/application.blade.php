b399b540c40e88c0285ca33577a23890371aaa5a
Dedy Akwety <dedyakwety@gmail.com>
2023-02-20 10:48:26 +0100

    code de meta referencement

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -15,6 +15,7 @@ $enfant = Pours::findOrFail(3);
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Colo Shop Template">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="RMQF1Z3jVsOZdL54akdBBNTt3MKMQuDhBBIe6ZnFM-c" />
 
 <!--meta http-equiv="refresh" content="15" /-->
 
